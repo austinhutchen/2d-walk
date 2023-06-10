590ca35131b4bf7a634eeb70566199f79a4fdd10
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-10 11:49:41 -0700

    small fixes

--- arduinogame.ino
+++ arduinogame.ino
@@ -7,40 +7,71 @@ const int Y_pin = A1; // analog pin connected to Y output
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 int px = 0;
 int py = 0;
+bool spawned = false;
+struct player *p = nullptr;
+struct map *m = nullptr;
 void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
   lcd.begin(16, 2);
 }
 
+struct player {
+  int px;
+  int py;
+};
+
+struct map {
+  char obj;
+};
+struct player *allocatep() {
+  struct player *p = malloc(sizeof(struct player));
+  return p;
+}
+struct map *allocatem() {
+  struct map *m =  malloc(sizeof(struct map));
+  return m;
+}
+void game(void *p, void *map) {
+  // makes player model
+}
+void clean(struct player *p, struct map *m) {
+  if (p && m) {
+    delete p;
+    delete m;
+  }
+  return;
+}
 void loop() {
 
-  lcd.setCursor(0, 0);
-  lcd.print(" X: ");
-  px = analogRead(X_pin);
-  py = analogRead(Y_pin);
-  lcd.print(px);
-  lcd.print(" --- ");
-  lcd.setCursor(8, 0);
-  lcd.print(" Y: ");
-  lcd.print(py);
-  lcd.setCursor(0, 1);
-  // player model will go here‰
+  while (true) {
+    if (spawned == false) {
+      struct player *p = allocatep();
+      struct map *m = allocatem();
+      spawned = true;
+    } else {
+    }
+    lcd.setCursor(0, 0);
+    lcd.print(" X: ");
+    px = analogRead(X_pin);
+    py = analogRead(Y_pin);
+    p->px = px;
+    p->py = py;
+    lcd.print(p->px);
+    lcd.print(" --- ");
+    lcd.setCursor(8, 0);
+    lcd.print(" Y: ");
+    lcd.print(p->py);
+    lcd.setCursor(0, 1);
+    // player model will go here‰
 
-  if (px < 1000 && py > 1000) {
-    // neither axis is 0
-    lcd.print('@');
+    if (px && py) {
+      lcd.print('@');
+    } else {
+      lcd.print("       ");
+      lcd.print('@');
+    }
   }
-  if (px > 1000 && py < 1000) {
-    // neither axis is 0
-    lcd.print('3');
-  } 
-  if(px > 1000 && py > 1000){
-    lcd.print('2');
-
-  }
-  else {
-    lcd.print("       ");
-    lcd.print('@');
-  }
+  clean(p, m);
+  return;
 }
