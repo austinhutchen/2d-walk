c1f74d090166da6c2b6b1527461f8e34bc6e5e84
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-25 09:09:16 -0700

    more small fixes

--- components/array.hpp
+++ components/array.hpp
@@ -11,20 +11,16 @@ public:
   char **map;
   matrix(player *_p, unsigned short rows, unsigned short cols) {
     this->p = _p;
-    char **a = new char *[rows];
+    this->map = new char *[rows];
     for (int i = 0; i < rows; ++i)
-      a[i] = new char[cols];
-    this->map = a;
-    spawn();
-  }
-
-  void spawn() {
+      this->map[i] = new char[cols];
     map[0][0] = p->character;
     p->row = 0;
     p->column = 0;
   }
+
   bool spawned() { return (p != 0x0); }
-  
+
   void right() {
     if (p->column < 16) {
       p->column += 1;
